fbd023c7eeff40a4f7254e97c0c5ece5968ee6e7
Vaibhav Mor <mor.vaibhav577@gmail.com>
2018-05-07 21:15:29 +1200

    Use TDD Methodology to implement Bank Deposit feature

---
+++ src/javaTDDBankApp/BankAccount.java
@@ -0,0 +1,12 @@
+package javaTDDBankApp;
+
+public class BankAccount {
+
+	public int balance;
+
+	public void deposit(int i) {
+		// TODO Auto-generated method stub
+		balance += i;
+	}
+
+}
---
+++ tests/javaTDDBankApp/BankAccountTest.java
@@ -0,0 +1,16 @@
+package javaTDDBankApp;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BankAccountTest {
+
+	@Test
+	void test() {
+		BankAccount acc = new BankAccount();
+		acc.deposit(50);
+		assertEquals(acc.balance, 50);
+	}
+
+}
